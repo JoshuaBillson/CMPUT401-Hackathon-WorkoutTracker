c89020194ab6d46bd821ffeb94e733d5d0c92b40
Raghav <raghav.chandak@indusnet.co.in>
2021-09-11 21:15:40 +0530

    Models added

--- workouts/models.py
+++ workouts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+class WorkoutLog(models.Model):
+    exercise = models.CharField(max_length=100)
+    duration = models.DecimalField(max_digits=3 , decimal_places=4)
+    date = models.DateField(default=datetime.date.today())
+
+class DietPlan(models.Model):
+    water = models.DecimalField(decimal_places=3 , max_digits=3)
+    date = models.DateField(auto_now_add=True)
+    protein = models.DecimalField(decimal_places=3 , max_digits=5)
+    carbohydrate = models.DecimalField(decimal_places=3 , max_digits=5)
+    fat = models.DecimalField(decimal_places=3, max_digits=5)
+    vitamin = models.DecimalField(decimal_places=3 , max_digits=5)
+    mineral = models.DecimalField(decimal_places=3 , max_digits=5)
+    roughage = models.DecimalField(decimal_places=3, max_digits=5)
